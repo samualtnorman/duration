26ce38d390b607f22cd0a7b402421132e25b14aa
Samual Norman <me@samual.uk>
2024-12-06 18:09:13 +0000

    only emit `@ts-self-types` when `--configJsr` is passed

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@
 import { rollupConfig } from "@samual/rollup-config"
 import Path from "path"
 
-export default rollupConfig({
+export default (/** @type {Record<string, unknown>} */ args) => rollupConfig(args.configJsr ? {
 	rollupOptions: { output: { banner: chunk => `// @ts-self-types="./${Path.basename(chunk.name)}.d.ts"` } },
 	terserOptions: { format: { comments: /@ts-self-types/ } }
-})
+} : undefined)
--- scripts/jsr.sh
+++ scripts/jsr.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 rm -rf dist
-./rollup.config.js
+./rollup.config.js --configJsr
 scripts/emit-types.sh
 scripts/emit-package-json.js
 scripts/emit-jsr-json.js
