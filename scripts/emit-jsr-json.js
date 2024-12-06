fd1ced75fb81dca09b8003bc193a53746453e8a5
Samual Norman <me@samual.uk>
2024-12-06 18:24:20 +0000

    emit `license` field in `jsr.json`

--- scripts/emit-jsr-json.js
+++ scripts/emit-jsr-json.js
@@ -2,8 +2,8 @@
 import { mkdirSync as makeDirectorySync, writeFileSync } from "fs"
 import packageJson from "../package.json" with { type: "json" }
 
-const { name, version } = packageJson
+const { name, version, license } = packageJson
 
 makeDirectorySync("dist", { recursive: true })
-writeFileSync("dist/jsr.json", JSON.stringify({ name, version, exports: `./index.js` }, undefined, "\t"))
+writeFileSync("dist/jsr.json", JSON.stringify({ name, version, exports: `./index.js`, license }, undefined, "\t"))
 process.exit()
